8a0a313f12bfe2904bcd4f8beacd9ae0fa0b9197
Oleksandr Zaliubovskyi <135022571+nekoduykod@users.noreply.github.com>
2023-12-11 21:38:55 +0200

    Update etl.py

--- etl.py
+++ etl.py
@@ -7,38 +7,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#Get pass from env var
 pwd = os.environ['PGPASS']
 uid = os.environ['PGUID']
-#Sql server db
 driver = "ODBC Driver 17 for SQL Server"
-#driver = "{SQL Server Native Client 11.0}"
 server = "COMPUTERVONSASC\SQLEXPRESS"
 database = "MSQL"
 
 def extract():
     try:
-        # SQL Server connection string
         connection_string = f"mssql+pyodbc://sasha:890@{server}/{database}?driver={driver}"  
         engine = create_engine(connection_string)
         table = 'dbo.nba_forecast'
-        #Query and load data into a dataframe
         query = f'SELECT * FROM {table}'
         df = pd.read_sql(query, engine)
-        #Process the data or perform any necessary operations
         load(df, table)
     except Exception as e:
         print("OH NO ERROR: " + str(e))
     finally:
-        # Close the connection
         engine.dispose()
          
 def load(df, table):
     try:
-        #Establish Postgre connection
         connection_string = f'postgresql://{uid}:{pwd}@localhost:5432/MyDB'
         engine = create_engine(connection_string)
-        #Specify table
         rows_imported = 0
         df.to_sql(f'stg_{table}', engine, if_exists='replace', index=False)
         rows_imported += len(df)
@@ -47,7 +38,6 @@ def load(df, table):
         print("Data load error: " + str(e))
  
 try:
-    #call extract function
     extract()
 except Exception as e:
     print("Error while extracting data: " + str(e))
